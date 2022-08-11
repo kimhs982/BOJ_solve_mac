9510488d0bb575f8ebde244fff843bba5bee5ed7
HyungSuk mac <kimhyungsuk982@gmail.com>
2022-08-11 19:17:49 +0900

    25304번(8월 11일)-영수증

---
+++ p25304.cpp
@@ -0,0 +1,22 @@
+// 25304번, 영수증
+#include <cstdio>
+
+int main()
+{
+    int X, N, a, b;
+    int sum = 0;
+
+    scanf("%d%d", &X, &N);
+    for(int i = 0; i < N; i++)
+    {
+        scanf("%d%d", &a, &b);
+        sum += (a * b);
+    }
+
+    if(X == sum)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
