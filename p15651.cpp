b114edd3135fbe33f840c8a20c99db4f26bbe9c7
HyungSuk mac <kimhyungsuk982@gmail.com>
2022-08-22 20:10:06 +0900

    15651번(8월 22일)-N과 M(3), 백트래킹

---
+++ p15651.cpp
@@ -0,0 +1,40 @@
+// 15651번, N과 M(3)
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::ios_base;
+
+int num_arr[7];
+
+void NandM(int, int, int);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    NandM(N, M, 0);
+
+    return 0;
+}
+
+// 백트래킹, 재귀함수
+void NandM(int in_N, int in_M, int height)
+{
+    if (height == in_M)
+    {
+        for (int i = 0; i < in_M; i++)
+            cout << num_arr[i] << ' ';
+        cout << '\n';
+    }
+    else
+        for (int i = 0; i < in_N; i++)
+        {
+            num_arr[height] = i + 1;
+            NandM(in_N, in_M, height + 1);
+        }
+}
